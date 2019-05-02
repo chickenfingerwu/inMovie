867a0eae44178159f24d43dafdbab0af98fca404
Vinh Quang Tran <starcraft6723@hotmail.com>
2019-05-02 18:32:22 +0700

    Added checkConnectivity and InstrumentedTest.

---
+++ app/src/androidTest/java/com/inmovie/inmovie/InstrumentedTest.java
@@ -0,0 +1,30 @@
+package com.inmovie.inmovie;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import com.inmovie.inmovie.model.api.CheckConnectivity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class InstrumentedTest {
+    @Test
+    public void testConnection() {
+        try {
+            assertEquals(7, new CheckConnectivity().execute().get().intValue());
+        }
+        catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
