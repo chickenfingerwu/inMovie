906b13a2e91877c88eaca890511771737a9e0722
chickenfingerwu <swlpham@gmail.com>
2019-05-08 07:54:13 +0700

    Added EpisodeDetails activity, can now display episode's details

--- app/src/main/java/com/inmovie/inmovie/TVclasses/Episode.java
+++ app/src/main/java/com/inmovie/inmovie/TVclasses/Episode.java
@@ -2,7 +2,35 @@ package com.inmovie.inmovie.TVclasses;
 
 import com.inmovie.inmovie.Movies;
 
+import java.io.Serializable;
+
 //Subclass to Movies since every episode is like a small movie
 public class Episode extends Movies {
+    private int tvID;
+    private int seasonNumber;
+    private int episodeNumber;
 
+    public int getSeasonNumber() {
+        return seasonNumber;
+    }
+
+    public int getTvID() {
+        return tvID;
+    }
+
+    public int getEpisodeNumber() {
+        return episodeNumber;
+    }
+
+    public void setSeasonNumber(int seasonNumber) {
+        this.seasonNumber = seasonNumber;
+    }
+
+    public void setTvID(int tvID) {
+        this.tvID = tvID;
+    }
+
+    public void setEpisodeNumber(int episodeNumber) {
+        this.episodeNumber = episodeNumber;
+    }
 }
